56da2a9c1815143abd31bd12de5ffe483df252c2
Ferris Boran <ferris.boran@gmail.com>
2019-03-08 21:53:14 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,3 +1,13 @@
 def badge_maker(name)
   return "Hello, my name is #{name}."
 end
+
+def batch_badge_creator(array)
+  room = 1
+  i = 0
+  while room < 8
+    return "Hello, #{array[i]}! You'll be assigned to room #{room}!"
+    i += 1
+    room += 1
+  end
+end
